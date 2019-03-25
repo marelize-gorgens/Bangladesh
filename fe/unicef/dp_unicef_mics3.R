44db74aaa9d5985938e049b012f9b7294bfc206a
Hellen <hellen.matarazzo@gmail.com>
2019-03-24 22:59:48 -0400

    MICS5 and MICS3 final extractions

---
+++ fe/unicef/dp_unicef_mics3.R
@@ -0,0 +1,177 @@
+#----------------------------------------------------------------------------------------------------------
+# dp_unicef_.R
+# Description: Data preprocessing of Unicef MICS3 datasets
+# Organization: Health, Nutrition, and Population (HNP) | The World Bank
+# Author: Hellen Matarazzo
+# Date: Updated on 03-24-2019
+#----------------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------------
+# Clear memory
+#----------------------------------------------------------------------------------------------------------
+
+rm(list=ls()) # Remove objects
+graphics.off # Close graphics
+cat("\014") # Clear console
+
+#----------------------------------------------------------------------------------------------------------
+# Load packages
+#----------------------------------------------------------------------------------------------------------
+
+if(!require(tidyverse)) installed.packages("tidyverse", dependencies=TRUE); library(tidyverse)
+if(!require(haven)) installed.packages("haven", dependencies=TRUE); library(haven)
+if(!require(rdhs)) installed.packages("rdhs"); library(rdhs)
+if(!require(survey)) installed.packages("survey", dependencies=TRUE); library(survey)
+if(!require(plyr)) installed.packages("plyr", dependencies=TRUE); library(plyr)
+if(!require(sjlabelled)) installed.packages("sjlabelled", dependencies=TRUE); library(sjlabelled)
+
+#----------------------------------------------------------------------------------------------------------
+# Load datasets
+#----------------------------------------------------------------------------------------------------------
+
+mics3files <- list.files("./data/unicef/MICS3", recursive=TRUE, pattern="*.sav", full.names=TRUE) # List files 
+mics3 <- sapply(mics3files,read_sav)
+names(mics3) <- c("ch3","hh3","hl3","wm3") # Rename lists
+
+#----------------------------------------------------------------------------------------------------------
+# Data transformation
+#----------------------------------------------------------------------------------------------------------
+
+# Children dataset
+ch3 <- mics3$ch3 # Load as data frame
+ch3_var <- get_variable_labels(ch3) # Get the data dictionary
+
+# Women dataset
+wm3 <- mics3$wm3 # Load as data frame
+wm3_var <- get_variable_labels(wm3) # Get the data dictionary
+
+#----------------------------------------------------------------------------------------------------------
+# Survey design
+#----------------------------------------------------------------------------------------------------------
+
+# ch3design <- svydesign(data=ch3, id=~HH1, weight=~chweight, strata=~HH7A) # Set survey design for dataset
+# sum(duplicated(ch3)) # Uniqueness: Check for duplicates
+# sum(is.na(ch3)) # Completeness: Check for NAs
+# na_ch3 <- data.frame(lapply(ch3, function(y) sum(length(which(is.na(y)))))) # Save NAs by column
+
+#----------------------------------------------------------------------------------------------------------
+# List of variables of interest
+#----------------------------------------------------------------------------------------------------------
+
+### Unit
+# HH7A      District
+# HH7       Division
+# chweight  Children's sample weight
+
+#----------------------------------------------------------------------------------------------------------
+# Data transformation
+#----------------------------------------------------------------------------------------------------------
+
+ch3 <- ch3 %>% filter(UF9 == 1) # Subset children actually interviewed
+wm3 <- wm3 %>% filter(WM7 == 1) # Subset women actually interviewed
+
+# Exclusive breastfeeding under 6 months
+table(ch3$cage) # Age <6
+attributes(ch3$BF1) # Child ever been breastfed ==1
+attributes(ch3$BF2) # Child still been breastfed ==1
+attributes(ch3$BF3B) # Child received plain water ==2
+attributes(ch3$BF3C) # Child received sweetened water or juice ==2
+attributes(ch3$BF3E) # Child received infant formula ==2
+attributes(ch3$BF3F) # Child received milk ==2
+attributes(ch3$BF3G) # Child received other liquids ==2
+attributes(ch3$BF3H) # Child received solid or mushy food ==2
+ch3$exclusive_breastfeeding <- ifelse(ch3$cage<6 & ch3$BF1==1 & ch3$BF2==1 & ch3$BF3B==2 & ch3$BF3C==2 & ch3$BF3E==2 &
+                                        ch3$BF3F==2 & ch3$BF3G==2 & ch3$BF3H==2, 1,
+                                      ifelse(ch3$cage<6, 0, NA))
+ 
+# Diarrhorea treatment via ORT
+attributes(ch3$CA1) # Child had diarrhoea in last 2 weeks ==1
+attributes(ch3$CA2A) # Drank fluid made from special packet (ORS) ==1
+attributes(ch3$CA2B) # Government-recommended homemade fluid ==1
+attributes(ch3$CA2C) # Pre-packaged ORS fluid for diarrhoea ==1
+attributes(ch3$CA3) # Child drank less or more during illness ==3
+ch3$diarrhoea_ors <- ifelse(ch3$CA1==1 & (ch3$CA2A==4 | ch3$CA2B==1 | ch3$CA2C==1 | ch3$CA3==3), 1,
+                            ifelse(ch3$CA1==1, 0, NA))
+
+# Care-seeking for children with ARI symptoms
+attributes(ch3$CA5) # Child ill with cough in last 2 weeks ==1
+attributes(ch3$CA6) # Difficulty breathing during illness with cough & ==1
+attributes(ch3$CA7) # Symptoms due to problem in chest or blocked nose & ==1 | 3
+attributes(ch3$CA9A) # Place sought care: Govt Hospital/Health centre
+attributes(ch3$CA9D) # Place sought care: Village health worker/HA
+attributes(ch3$CA9E) # Place sought care: Mobile/outreach clinic
+attributes(ch3$CA9H) # Place sought care: Other public source
+attributes(ch3$CA9I) # Place sought care: Private hospital/clinic
+attributes(ch3$CA9J) # Place sought care: Private physician
+attributes(ch3$CA9O) # Place sought care: Other private medical
+attributes(ch3$CA9S) # Place sought care: NGO hospital/clinic
+ch3$ari_care <- ifelse((ch3$CA5==1 & ch3$CA6==1 & (ch3$CA7==1 | ch3$CA7==3)) &
+                         (ch3$CA9A=="A" | ch3$CA9D=="D" | ch3$CA9E=="E" | ch3$CA9H=="H" |
+                            ch3$CA9I=="I" | ch3$CA9J=="J" | ch3$CA9O=="O" | ch3$CA9S=="S"), 1,
+                       ifelse(ch3$CA5==1 & ch3$CA6==1 & (ch3$CA7==1 | ch3$CA7==3), 0, NA))
+
+# Antibiotics treatment for ARI symptoms
+attributes(ch3$CA10) # Given medicine to treat this illness ==1
+attributes(ch3$CA11A) # Medicine: Antibiotic pill / syrup =="A"
+ch3$ari_antibiotics <- ifelse((ch3$CA5==1 & ch3$CA6==1 & (ch3$CA7==1 | ch3$CA7==3)) & ch3$CA11A=="A", 1,
+                              ifelse(ch3$CA5==1 & ch3$CA6==1 & (ch3$CA7==1 | ch3$CA7==3), 0, NA))
+
+# Antenatal care coverage
+attributes(wm3$CM12) # Live birth in last 2 years =="Y"
+attributes(wm3$MN2A) # Antenatal care: Doctor
+attributes(wm3$MN2B) # Antenatal care: Nurse / Midwife
+attributes(wm3$MN2F) # Antenatal care: Traditional birth attendant
+attributes(wm3$MN2G) # Antenatal care: Community health worker (FWA/HA/MA)
+attributes(wm3$MN2X) # Antenatal care: Other
+wm3$antenatal <- ifelse(wm3$CM12=="Y" & (wm3$MN2A=="A"| wm3$MN2B=="B"| wm3$MN2F=="F"| wm3$MN2G=="G"| wm3$MN2X=="X"), 1,
+                        ifelse(wm3$CM12=="Y", 0, NA))
+
+# Skilled attendant at delivery
+attributes(wm3$CM12) # Last birth in last two years =="Y"
+attributes(wm3$MN7A) # Assistance at delivery: Doctor
+attributes(wm3$MN7B) # Assistance at delivery: Nurse / Midwife
+wm3$attendant_delivery <- ifelse(wm3$CM12=="Y" & (wm3$MN7A=="A" | wm3$MN7B=="B"), 1,
+                                 ifelse(wm3$CM12=="Y", 0, NA))
+
+# Institutional deliveries
+attributes(wm3$CM12) # Last birth in last two years =="Y"
+attributes(wm3$MN8) # Place of delivery ==21, 22, 23, 26, 31, 32, 33, 36
+wm3$institutional_delivery <- ifelse(wm3$CM12=="Y" & wm3$MN8 %in% c(21, 22, 23, 26, 31, 32, 33, 36), 1, 
+                                     ifelse(wm3$CM12=="Y", 0, NA))
+
+#----------------------------------------------------------------------------------------------------------
+# Create final dataset
+#----------------------------------------------------------------------------------------------------------
+
+final1 <- ddply(ch3, ~HH7A, summarise, # Summarize by district
+                exclusive_breastfeeding=weighted.mean(exclusive_breastfeeding, chweight, na.rm=TRUE),
+                diarrhoea_ors=weighted.mean(diarrhoea_ors, chweight, na.rm=TRUE),
+                ari_care=weighted.mean(ari_care, chweight, na.rm=TRUE),
+                ari_antibiotics=weighted.mean(ari_antibiotics, chweight, na.rm=TRUE))
+
+final2 <- ddply(wm3, ~HH7A, summarise, # Summarize by district
+                antenatal=weighted.mean(antenatal, wmweight, na.rm=TRUE),
+                attendant_delivery=weighted.mean(attendant_delivery, wmweight, na.rm=TRUE),
+                institutional_delivery=weighted.mean(institutional_delivery, wmweight, na.rm=TRUE))
+
+final <- merge(final1, final2)
+names(final)[1] <- "District"
+final$District <- get_labels(wm3$HH7A)
+ 
+#----------------------------------------------------------------------------------------------------------
+# Save final dataset
+#----------------------------------------------------------------------------------------------------------
+
+write.csv(final,"./output/unicef/data/data_unicef_mics3_2006.csv", row.names=FALSE) # Save metadata
+
+#----------------------------------------------------------------------------------------------------------
+# Create/save metadata
+#----------------------------------------------------------------------------------------------------------
+
+meta_mics3 <- data.frame()
+for (i in seq_along(mics3)){
+  meta <- data.frame("Source"="MICS3", "File"= names(mics3[i]),"Variable"=colnames(mics3[[i]]),
+                     "Description"= sapply(mics3[[i]], function(x) attributes(x)$label))
+  meta_mics3 <- rbind(meta_mics3,meta)
+}
+write.csv(meta_mics3,"./output/unicef/data/metadata_unicef_mics3.csv", row.names=FALSE) # Save metadata
--- fe/unicef/dp_unicef_mics5.R
+++ fe/unicef/dp_unicef_mics5.R
@@ -3,7 +3,7 @@
 # Description: Data preprocessing of Unicef MICS5 datasets
 # Organization: Health, Nutrition, and Population (HNP) | The World Bank
 # Author: Hellen Matarazzo
-# Date: Updated on 03-20-2019
+# Date: Updated on 03-24-2019
 #----------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------------
@@ -23,6 +23,7 @@ if(!require(haven)) installed.packages("haven", dependencies=TRUE); library(have
 if(!require(rdhs)) installed.packages("rdhs"); library(rdhs)
 if(!require(survey)) installed.packages("survey", dependencies=TRUE); library(survey)
 if(!require(plyr)) installed.packages("plyr", dependencies=TRUE); library(plyr)
+if(!require(sjlabelled)) installed.packages("sjlabelled", dependencies=TRUE); library(sjlabelled)
 
 #----------------------------------------------------------------------------------------------------------
 # Load datasets
@@ -44,23 +45,22 @@ ch5_var <- get_variable_labels(ch5) # Get the data dictionary
 wm5 <- mics5$wm5 # Load as data frame
 wm5_var <- get_variable_labels(wm5) # Get the data dictionary
 
-# Household dataset
-hh5 <- mics5$hh5 # Load as data frame
-hh5_var <- get_variable_labels(hh5) # Get the data dictionary
-
-# Household members dataset
-hl5 <- mics5$hl5 # Load as data frame
-hl5_var <- get_variable_labels(hl5) # Get the data dictionary
+# # Household dataset
+# hh5 <- mics5$hh5 # Load as data frame
+# hh5_var <- get_variable_labels(hh5) # Get the data dictionary
+# 
+# # Household members dataset
+# hl5 <- mics5$hl5 # Load as data frame
+# hl5_var <- get_variable_labels(hl5) # Get the data dictionary
 
 #----------------------------------------------------------------------------------------------------------
 # Survey design
 #----------------------------------------------------------------------------------------------------------
 
-ch5design <- svydesign(data=ch5, id=~HH1, weight=~chweight, strata=~HH7A) # Set survey design for dataset
-sum(duplicated(ch5)) # Uniqueness: Check for duplicates
-sum(is.na(ch5)) # Completeness: Check for NAs
-na_ch5 <- data.frame(lapply(ch5, function(y) sum(length(which(is.na(y)))))) # Save NAs by column
-attach(ch5)
+# ch5design <- svydesign(data=ch5, id=~HH1, weight=~chweight, strata=~HH7A) # Set survey design for dataset
+# sum(duplicated(ch5)) # Uniqueness: Check for duplicates
+# sum(is.na(ch5)) # Completeness: Check for NAs
+# na_ch5 <- data.frame(lapply(ch5, function(y) sum(length(which(is.na(y)))))) # Save NAs by column
 
 #----------------------------------------------------------------------------------------------------------
 # List of variables of interest
@@ -79,22 +79,22 @@ wm5 <- wm5 %>% filter(WM7 == 1) # Subset women actually interviewed
 
 # Underweight prevalence 
 attributes(ch5$WAZ2) # Weight for age
-ch5$underweight <- ifelse(ch5$WAZ2==99.97 | ch5$WAZ2==99.98 | ch5$WAZ2==99.99, NA,
-                          ifelse(ch5$WAZ2<(-2)*mad(ch5$WAZ2[ch5$WAZ2<=5]),1,0))
+ch5$underweight <- ifelse(ch5$WAZ2 < (-2), 1,
+                          ifelse(!(ch5$WAZ2 %in% c(99.97, 99.98, 99.99)), 0, NA))
 
 # Stuning prevalence 
 attributes(ch5$HAZ2) # Height for age
-ch5$stuning <- ifelse(ch5$HAZ2==99.97 | ch5$HAZ2==99.98 | ch5$HAZ2==99.99, NA,
-                          ifelse(ch5$HAZ2<(-2)*mad(ch5$HAZ2[ch5$HAZ2<=5]),1,0))
+ch5$stuning <- ifelse(ch5$HAZ2 < (-2), 1,
+                      ifelse(!(ch5$HAZ2 %in% c(99.97, 99.98, 99.99)), 0, NA))
 
 # Wasting prevalence
 attributes(ch5$WHZ2) # Weight for height
-ch5$wasting <- ifelse(ch5$WHZ2==99.97 | ch5$WHZ2==99.98 | ch5$WHZ2==99.99, NA,
-                         ifelse(ch5$WHZ2<(-2)*mad(ch5$WHZ2[ch5$WHZ2<=5]),1,0))
+ch5$wasting <- ifelse(ch5$WHZ2 < (-2), 1,
+                      ifelse(!(ch5$WHZ2 %in% c(99.97, 99.98, 99.99)), 0, NA))
 
 # Overweight prevalence
-ch5$overweight <- ifelse(ch5$WHZ2==99.97 | ch5$WHZ2==99.98 | ch5$WHZ2==99.99, NA,
-                          ifelse(ch5$WHZ2>2*mad(ch5$WHZ2[ch5$WHZ2<=5]),1,0))
+ch5$overweight <- ifelse(ch5$WHZ2 > 2 & !(ch5$WHZ2 %in% c(99.97, 99.98, 99.99)), 1,
+                         ifelse(!(ch5$WHZ2 %in% c(99.97, 99.98, 99.99)), 0, NA))
 
 # Exclusive breastfeeding under 6 months
 attributes(ch5$CAGE) # Age
@@ -109,11 +109,9 @@ attributes(ch5$BF12) # Child drank any other liquid yesterday ==2
 attributes(ch5$BF13) # Child drank or ate yogurt yesterday ==2
 attributes(ch5$BF15) # Child ate thin porridge yesterday ==2
 attributes(ch5$BF16) # Child ate solid or semi-solid food yesterday ==2
-attributes(ch5$BF18) # Child drank anything else from the bottle with a nipple yesterday ==2
-ch5$exclusive_breastfeeding <- ifelse(ch5$CAGE>=6, NA,
-                                      ifelse(ch5$BF1==1 & ch5$BF2==1 & ch5$BF3==2 & ch5$BF4==2 & 
-                                               ch5$BF6==2 & ch5$BF8==2 & ch5$BF9==2 & ch5$BF12==2 & 
-                                               ch5$BF13==2 & ch5$BF15==2 & ch5$BF16==2 & ch5$BF18==2, 1, 0))
+ch5$exclusive_breastfeeding <- ifelse(ch5$CAGE<6 & ch5$BF1==1 & ch5$BF2==1 & ch5$BF3==2 & ch5$BF4==2 & ch5$BF6==2 & ch5$BF8==2 & 
+                                        ch5$BF9==2 & ch5$BF12==2 & ch5$BF13==2 & ch5$BF15==2 & ch5$BF16==2, 1,
+                                      ifelse(ch5$CAGE<6, 0, NA))
 
 # Diarrhorea treatment via ORT
 attributes(ch5$CA1) # Child had diarrhoea in last 2 weeks ==1
@@ -124,15 +122,15 @@ attributes(ch5$CA4C) # Sugar and salt solution for diarrhoea ==1
 attributes(ch5$CA4D) # Green coconut water for diarrhoea ==1
 attributes(ch5$CA4E) # Rice water for diarrhoea ==1
 attributes(ch5$CA4F) # Boiled rice water for diarrhoea ==1
-ch5$diarrhoeaors <- ifelse(ch5$CA1!=1, NA,
-                           ifelse(ch5$CA2==4 | ch5$CA4A==1 | ch5$CA4B==1 | ch5$CA4C==1 | ch5$CA4D==1 | 
-                                                  ch5$CA4E==1 | ch5$CA4F==1,1,0))
+ch5$diarrhoea_ors <- ifelse(ch5$CA1==1 & (ch5$CA2==4 | ch5$CA4A==1 | ch5$CA4B==1 | ch5$CA4C==1 | ch5$CA4D==1 | 
+                                    ch5$CA4E==1 | ch5$CA4F==1), 1,
+                           ifelse(ch5$CA1==1, 0, NA))
 
 # Care-seeking for children with ARI symptoms
 attributes(ch5$CA7) # Child ill with cough in last 2 weeks ==1
 attributes(ch5$CA8) # Difficulty breathing during illness with cough & ==1
 attributes(ch5$CA9) # Symptoms due to problem in chest or blocked nose & ==1 | 3
-ch5$aricare <- ifelse((ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3)) & 
+ch5$ari_care <- ifelse((ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3)) & 
                         (ch5$CA11A=="A" | ch5$CA11B=="B" | ch5$CA11C=="C" | ch5$CA11D=="D" | 
                          ch5$CA11E=="E" | ch5$CA11H=="H" | ch5$CA11I=="I" | ch5$CA11J=="J" | 
                          ch5$CA11L=="L" | ch5$CA11O=="O"), 1, 
@@ -142,52 +140,81 @@ ch5$aricare <- ifelse((ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3)) &
 attributes(ch5$CA12) # Given medicine to treat this illness ==1
 attributes(ch5$CA13A) # Medicine: Antibiotic pill / syrup =="A"
 attributes(ch5$CA13B) # Medicine: Antibiotic injection ==B
-ch5$ariantibiotics <- ifelse((ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3)) & 
+ch5$ari_antibiotics <- ifelse((ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3)) & 
                                (ch5$CA13A=="A" | ch5$CA13B=="B"), 1, 
                              ifelse(ch5$CA7==1 & ch5$CA8==1 & (ch5$CA9==1 | ch5$CA9==3), 0, NA))
 
-# Adolescent birth rate
-
 # Contraceptive prevalence
 attributes(wm5$MA1) # Currently married ==1
+attributes(wm5$CP2) # Currently using a method to avoid pregnancy ==1
+wm5$contraception <- ifelse(wm5$MA1==1 & wm5$CP2==1, 1,
+                            ifelse(wm5$MA1==1, 0, NA))
 
 # Antenatal care coverage
 attributes(wm5$CM13) # Last birth in last two years =="Y"
+attributes(wm5$MN1) # Received antenatal care
+attributes(wm5$MN3) # Times received antenatal care >=4 !=98 !=99
+attributes(wm5$MN2A) # Antenatal care: Doctor
+attributes(wm5$MN2B) # Antenatal care: Nurse / Midwife
+attributes(wm5$MN2C) # Antenatal care: Auxiliary midwife
+attributes(wm5$MN2F) # Antenatal care: Traditional birth attendant
+attributes(wm5$MN2G) # Antenatal care: Community health worker
+attributes(wm5$MN2X) # Antenatal care: Other
+wm5$antenatal <- ifelse(wm5$CM13=="Y" & wm5$MN1==1 & (wm5$MN3>=4 & wm5$MN3!=98 & wm5$MN3!=99) &
+                        (wm5$MN2A=="A"| wm5$MN2B=="B"| wm5$MN2C=="C"| wm5$MN2F=="F"| wm5$MN2G=="G"| wm5$MN2X=="X"), 1,
+                        ifelse(wm5$CM13=="Y", 0, NA))
 
 # Skilled attendant at delivery
+attributes(wm5$CM13) # Last birth in last two years =="Y"
+attributes(wm5$MN17A) # Assistance at delivery: Doctor
+attributes(wm5$MN17B) # Assistance at delivery: Nurse / Midwife
+attributes(wm5$MN17C) # Assistance at delivery: Auxiliary midwife
+wm5$attendant_delivery <- ifelse(wm5$CM13=="Y" & (wm5$MN17A=="A" | wm5$MN17B=="B" | wm5$MN17C=="C"), 1, 
+                                ifelse(wm5$CM13=="Y", 0, NA))
 
 # Institutional deliveries
+attributes(wm5$CM13) # Last birth in last two years =="Y"
+attributes(wm5$MN18) # Place of delivery ==21, 22, 23, 26, 31, 32, 33, 36
+wm5$institutional_delivery <- ifelse(wm5$CM13=="Y" & (wm5$MN18==21 | wm5$MN18==22 | wm5$MN18==23 | wm5$MN18==26 |
+                                                       wm5$MN18==31 | wm5$MN18==32 | wm5$MN18==33 | wm5$MN18==36), 1, 
+                                ifelse(wm5$CM13=="Y", 0, NA))
 
 # C-section
-
+attributes(wm5$CM13) # Last birth in last two years =="Y"
+attributes(wm5$MN19) # Delivery by caesarean section ==1
+wm5$caesarean <- ifelse(wm5$CM13=="Y" & wm5$MN19==1, 1,
+                       ifelse(wm5$CM13=="Y", 0, NA))
 
 #----------------------------------------------------------------------------------------------------------
 # Create final dataset
 #----------------------------------------------------------------------------------------------------------
-final <- ddply(ch5, ~HH7A, summarise, # Summarize by district
+
+final1 <- ddply(ch5, ~HH7A, summarise, # Summarize by district
                    underweight=weighted.mean(underweight, chweight, na.rm=TRUE),
                    stuning=weighted.mean(stuning, chweight, na.rm=TRUE),
                    wasting=weighted.mean(wasting, chweight, na.rm=TRUE),
                    overweight=weighted.mean(overweight, chweight, na.rm=TRUE),
                    exclusive_breastfeeding=weighted.mean(exclusive_breastfeeding, chweight, na.rm=TRUE),
-                   diarrhoeaors=weighted.mean(diarrhoeaors, chweight, na.rm=TRUE),
-                   aricare=weighted.mean(aricare, chweight, na.rm=TRUE),
-                   ariantibiotics=weighted.mean(ariantibiotics, chweight, na.rm=TRUE))
-# weighted.mean(ch5$ariantibiotics, ch5$chweight, na.rm=TRUE)
-# mean(ch5$underweight, na.rm=TRUE)
-# svymean(~underweight, design=ch5design, na.rm=T)
-# nrow(ch5[ch5$underweight == 1,])
-# length(ch5$underweight[ch5$underweight == 1])
-# sum(which(ch5$underweight == 1))
-# sum(is.na(ch5$ariantibiotics))
-# table(ch5$ariantibiotics)
+                   diarrhoea_ors=weighted.mean(diarrhoea_ors, chweight, na.rm=TRUE),
+                   ari_care=weighted.mean(ari_care, chweight, na.rm=TRUE),
+                   ari_antibiotics=weighted.mean(ari_antibiotics, chweight, na.rm=TRUE))
+
+final2 <- ddply(wm5, ~HH7A, summarise, # Summarize by district
+                contraception=weighted.mean(contraception, wmweight, na.rm=TRUE),
+                antenatal=weighted.mean(antenatal, wmweight, na.rm=TRUE),
+                attendant_delivery=weighted.mean(attendant_delivery, wmweight, na.rm=TRUE),
+                institutional_delivery=weighted.mean(institutional_delivery, wmweight, na.rm=TRUE),
+                caesarean=weighted.mean(caesarean, wmweight, na.rm=TRUE))
+
+final <- merge(final1, final2)
+names(final)[1] <- "District"
+final$District <- get_labels(wm5$HH7A)
 
 #----------------------------------------------------------------------------------------------------------
 # Save final dataset
 #----------------------------------------------------------------------------------------------------------
 
 write.csv(final,"./output/unicef/data/data_unicef_mics5_2012.csv", row.names=FALSE) # Save metadata
-# write.csv(ch5,"C:/Users/wb531612/Desktop/ch5.csv", row.names=FALSE) # Save dataset
 
 #----------------------------------------------------------------------------------------------------------
 # Create/save metadata
